8fb98abef903c08554d292ed7bd1556096195615
arianitSadriu <122325048+arianitSadriu@users.noreply.github.com>
2025-11-23 14:26:40 +0100

    Working on controllers

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Application;
+use App\Models\Post;
+use App\Models\User;
 
 class AdminController extends Controller
 {
-    //
+    
+    public function deleteUser(User $user){
+
+        $user->delete();
+        return Redirect::back()->with('success', 'User has been deleted');
+
+    }
+
+    public function deletePost(Post $post){
+        $post->delete();
+        return Redirect::back()->with('success', 'Post has been deleted');
+    }
+
 }
