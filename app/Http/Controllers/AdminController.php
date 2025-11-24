aa5e349ba165a04f208021e34bc5ca5c4d9c26da
arianitSadriu <122325048+arianitSadriu@users.noreply.github.com>
2025-11-24 02:05:40 +0100

    Initializing other controllers

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+<<<<<<< Updated upstream
 use App\Models\Application;
 use App\Models\Post;
 use App\Models\User;
@@ -9,6 +10,46 @@ use App\Models\User;
 class AdminController extends Controller
 {
     
+=======
+<<<<<<< Updated upstream
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    //
+=======
+use App\Models\Application;
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+use Inertia\Inertia;
+
+class AdminController extends Controller
+{
+     public function index()
+    {
+        $EmployeeCount = User::where('role' , 'employee')->count();
+        $EmployerCount = User::where('role' , 'employer')->count();
+        $PostCount = Post::all()->count();
+        $ApplicationCount = Application::all()->count();
+        $Users = User::with('city')->get();
+        $Posts = Post::with('user','company')->get();
+        $accepted = Application::where('status' , 'accepted')->count();
+        $rejected = Application::where('status' , 'rejected')->count();
+
+        return Inertia::render('Dashboard', [
+            'EmployeeCount' => $EmployeeCount,
+            'EmployerCount' => $EmployerCount,
+            'PostCount' => $PostCount,
+            'ApplicationCount' => $ApplicationCount,
+            'Users' => $Users,
+            'Posts' => $Posts,
+            'accepted' => $accepted,
+            'rejected' => $rejected,
+        ]);
+    }
+>>>>>>> Stashed changes
     public function deleteUser(User $user){
 
         $user->delete();
@@ -21,4 +62,8 @@ class AdminController extends Controller
         return Redirect::back()->with('success', 'Post has been deleted');
     }
 
+<<<<<<< Updated upstream
+=======
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 }
